09192a3f5ab33963f1032de9ce677c143c86e5dd
Eduardo Londero <eduardolonderovieira@gmail.com>
2022-05-30 18:30:54 +0000

    Add /logout controller

---
+++ src/main/java/br/ufsm/csi/poowi/controllers/LogoutController.java
@@ -0,0 +1,34 @@
+package br.ufsm.csi.poowi.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.ufsm.csi.poowi.model.User;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            session.setAttribute("error", "Não logado");
+
+            resp.sendRedirect(req.getContextPath() + "/login");
+
+            return;
+        }
+
+        session.removeAttribute("user");
+
+        resp.sendRedirect(req.getContextPath() + "/home");
+    }
+}
