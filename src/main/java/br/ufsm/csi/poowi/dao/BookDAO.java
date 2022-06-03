df4cdae3d76b11b7e8c8014291d98c082b8c995d
Eduardo Londero <eduardolonderovieira@gmail.com>
2022-06-03 02:32:05 +0000

    Add Book DAO

---
+++ src/main/java/br/ufsm/csi/poowi/dao/BookDAO.java
@@ -0,0 +1,90 @@
+package br.ufsm.csi.poowi.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.ufsm.csi.poowi.model.Book;
+
+public class BookDAO {
+    private Book fromResultSet(ResultSet resultSet) throws SQLException {
+        Book book = new Book();
+
+        book.setId(resultSet.getInt("id"));
+        book.setName(resultSet.getString("name"));
+        book.setSynopsis(resultSet.getString("synopsis"));
+        book.setPages(resultSet.getInt("pages"));
+        book.setChapters(resultSet.getFloat("chapters"));
+        book.setAuthor(resultSet.getString("author"));
+        book.setPublisher(resultSet.getString("publisher"));
+        book.setYear(resultSet.getInt("year"));
+        book.setCover(resultSet.getString("cover"));
+
+        return book;
+    }
+
+    public Book getBook(int id) {
+        Book book = null;
+
+        try (Connection con = new DBConnect().getConnection()) {
+            String sql = "SELECT * FROM books WHERE id = ?;";
+
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                book = this.fromResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return book;
+    }
+
+    public boolean createBook(String name, String author) {
+        try (Connection con = new DBConnect().getConnection()) {
+            // possível bug: registrar o mesmo livro
+            // (pode existir dois autores que fizeram um livro com mesmo nome)
+            String sql = "INSERT INTO books (name, author) VALUES (?, ?)";
+
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, author);
+
+            preparedStatement.execute();
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public boolean updateBook(Book newBook) {
+        try (Connection con = new DBConnect().getConnection()) {
+            String sql = "UPDATE books SET name = ?, synopsis = ?, pages = ?, chapters = ?, author = ?, publisher = ?, year = ?";
+
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, newBook.getName());
+            preparedStatement.setString(2, newBook.getSynopsis());
+            preparedStatement.setInt(3, newBook.getPages());
+            preparedStatement.setFloat(4, newBook.getChapters());
+            preparedStatement.setString(5, newBook.getAuthor());
+            preparedStatement.setString(6, newBook.getPublisher());
+            preparedStatement.setInt(7, newBook.getYear());
+
+            preparedStatement.execute();
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
